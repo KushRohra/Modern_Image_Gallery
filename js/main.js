ca7cc6fa174bafc5c0c363713d0cabefd9398802
KushRohra <iec2017024@iiita.ac.in>
2020-02-04 02:22:11 +0530

    Animation added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
 const opacity = 0.5;
 
+//Set first img opacity
+imgs[0].style.opacity = opacity;
+
 imgs.forEach( img => img.addEventListener('click', imgClick));
 
 function imgClick(e)
@@ -11,6 +14,12 @@ function imgClick(e)
 
     //Change current to src of clicked image
     current.src = e.target.src;
+
+    //Add fade-in Class
+    current.classList.add('fade-in');
+
+    //Remove fade-in class after 0.5s
+    setTimeout( () => current.classList.remove('fade-in'), 500);
 
     //Change the opacity 
     e.target.style.opacity = opacity;
